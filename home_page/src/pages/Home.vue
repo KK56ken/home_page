6d074cf316e12d345e4eeb8becdd66da75b55547
Ksub77 <kiido1612@icloud.com>
2020-12-20 20:58:30 +0900

    [ADD]module追加

---
+++ home_page/src/pages/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+        homeだよ
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style>
+</style>
