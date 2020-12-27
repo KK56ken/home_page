57a3d92838280cf65f09af294a0748004ff12c88
Sken <k18056kk@aitech.ac.jp>
2020-12-28 03:58:13 +0900

    [update]2017年モデルMac book Airの画面サイズのみ対応させた

---
+++ home_page/src/components/Osero.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-img
+      :height="this.height"
+      class="opacity_fadein"
+      src="../assets/osero.png"
+      style="opacity:0.8"
+    >
+    </v-img>
+</template>
+<script>
+export default {
+  data:()=>({
+    scrollY:0,
+    height:0,
+  }),
+  mounted() {
+    this.height = window.innerHeight
+  }
+}
+
+</script>
+<style>
+@keyframes op_fadeIn{
+    0% {
+        opacity:0;
+    }
+    100% {
+        opacity:0.8;
+    }
+}
+.opacity_fadein{
+  animation:op_fadeIn 10s;
+  /* animation-iteration-count:infinite; */
+}
+</style>
--- home_page/src/pages/Home.vue
+++ home_page/src/pages/Home.vue
@@ -1,72 +1,85 @@
 <template>
-  <v-container>
-    <Header />
-    <v-row>
-      <v-col cols="6">
-        <v-img
-          class="fadein"
-          src="../assets/black_name_rogo.png"
-        >
-        </v-img>
-      </v-col>
-      <v-col cols="6">
-        <v-treeview
-        class="mt-6 pa-2 slidefadeIn"
-        dense
-        rounded
-        hoverable
-        activatable
-        off-icon
-        :items="items"
-        ></v-treeview>
-      </v-col>
-    </v-row>
-  </v-container>
+  <div class="Home">
+    <!-- class="pt-6" -->
+    <div class="pt-6" style="height:789px">
+      <v-row>
+        <Header />
+        <v-col cols="6">
+          <v-img
+            class="pa-6 fadein"
+            src="../assets/black_name_rogo.png"
+          >
+          </v-img>
+        </v-col>
+        <v-col cols="6" >
+          <v-treeview
+          class="pa-4 mt-6 slidefadeIn"
+          dense
+          rounded
+          hoverable
+          activatable
+          off-icon
+          :items="items"
+          ></v-treeview>
+        </v-col>
+      </v-row>
+    </div>
+    <OseroApp />
+  </div>
 </template>
 
 <script>
-import Header from '../components/Header';
+import Header from "@/components/Header.vue";
+import OseroApp from '../components/Osero.vue';
+
 export default {
-  data:()=>({
-    scrollY:0,
-    items: [
-        {
-          id: 1,
-          name: 'Applications :',
-          children: [
-            { id: 2, name: 'オンライン対戦　3Dオセロ' },
-            { id: 3, name: 'Chrome : app' },
-            { id: 4, name: 'more' },
-          ],
-        },
-        {
-          id: 5,
-          name: 'Portfolio',
-        },
-        {
-          id: 6,
-          name: 'Blog :',
-          children: [
-            { id: 7, name: 'VPN設定' },
-            { id: 8, name: 'WEBサーバ設定' },
-            { id: 9, name: 'more' },
-          ],
-        },
-        {
-          id: 10,
-          name: 'SNS :',
-          children: [
-            { id: 11, name: 'Git Hub : KK56ken' },
-            { id: 12, name: 'Twitter : kkensuke_suzuki' },
-          ],
-        },
-      ],
-  }),
+  name: "Home",
   components: {
     Header,
+    OseroApp,
   },
+  data:()=>({
+    scrollY:0,
+    height:0,
+    items: [
+      {
+        id: 1,
+        name: 'Applications :',
+        children: [
+          { id: 2, name: 'オンライン対戦　3Dオセロ' },
+          { id: 3, name: '料理検索' },
+          { id: 4, name: 'more' },
+        ],
+      },
+      {
+        id: 5,
+        name: 'Portfolio　リニューアル中',
+      },
+      {
+        id: 6,
+        name: 'Blog :',
+        children: [
+          { id: 7, name: 'VPN設定' },
+          { id: 8, name: 'WEBサーバ設定' },
+          { id: 9, name: 'more' },
+        ],
+      },
+      {
+        id: 10,
+        name: 'SNS :',
+        children: [
+          { id: 11, name: 'Git Hub : KK56ken' },
+          { id: 12, name: 'Twitter : kkensuke_suzuki' },
+        ],
+      },
+    ],
+  }),
   mounted() {
     window.addEventListener('scroll', this.handleScroll);
+  },
+  created() {
+    this.height = window.innerHeight
+    console.log(this.height)
   },
   methods: {
     handleScroll() {
